f42f46aa2d4bbff3478ce8e0594d055301645a67
shaanardev <123060363+shaanardev@users.noreply.github.com>
2023-02-11 11:16:19 +1100

    Add Form Validation to Contact Form (#8)
    * Update TextInput - Add on input event dispatcher | Add required attribute prop
    
    * Add basic form validation to ContactForm | Add new FormItem Type | Update how input elements are generated/rendered
    
    * Address feedback - Remove uncessary complexity | Refactor invalid form message to now fade in and out | Update text colour for better contrast
    
    * Move error message logic into TextInput to remove the gap caused by flex gap between it and the input element.

--- src/components/contactForm/ContactForm.svelte
+++ src/components/contactForm/ContactForm.svelte
@@ -1,42 +1,92 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import TextBoxInput from './TextBoxInput.svelte';
 	import TextInput from './TextInput.svelte';
+
 	export let page: string;
 
-	let name: string;
-	let email: string;
-	let message: string;
-	let sent = false;
+	let formSent = false;
+	let showErrorMessage = false;
+
+	let nameFormItem = {
+		fieldName: 'Name',
+		value: '',	
+		errorMessage: 'Please tell us who you are.'
+	}
+
+	let emailFormItem = {
+		fieldName: 'Email',
+		value: '',
+		errorMessage: 'Please let us know how to contact you.',
+	}
+
+	let messageFormItem = {
+		fieldName: 'Message',
+		value: '',
+	}
+
+	const handleInput = () => showErrorMessage = false;
+
+	const validateInput = (value: string) => value !== undefined && value.length > 0;
+	
+	const validateForm = () => {
+		let isValidEmail = validateInput(emailFormItem.value);
+		let isValidName = validateInput(nameFormItem.value);
+
+		return isValidEmail && isValidName;
+	};
+
+	const sendFormData = async () => {
+		if (!formSent) {
+			fetch('/api/contact', {
+				method: 'post',
+				body: JSON.stringify({
+					data: {
+						name: nameFormItem.value,
+						email: emailFormItem.value,
+						message: messageFormItem.value,
+						page
+					}
+				})
+			});
+			formSent = true;
+		}
+	};
 
 	async function submit(e: Event): Promise<void> {
-		if (sent) {
+		let isFormValid = validateForm();
+		
+		if(!isFormValid) {
+			showErrorMessage = true;
 			return;
 		}
-		e.preventDefault();
-		fetch('/api/contact', {
-			method: 'post',
-			body: JSON.stringify({
-				data: {
-					name,
-					email,
-					message,
-					page
-				}
-			})
-		});
-		sent = true;
+			
+		if (!formSent) {
+			return await sendFormData();
+		} 
 	}
 </script>
 
 <form on:submit={submit}>
 	<h3>Contact us</h3>
-	<TextInput fieldName="Name" bind:value={name} />
-	<TextInput fieldName="Email" bind:value={email} />
 
-	<TextBoxInput fieldName="Message" bind:value={message} />
-	<button on:click={submit} type="submit" class={sent ? 'sent' : ''}
-		>{sent ? 'Thank You' : 'Submit'}</button
-	>
+	<TextInput
+	on:input={handleInput}
+	showErrorMessage={showErrorMessage}
+	{...nameFormItem}
+	/>
+	
+	<TextInput
+	on:input={handleInput}
+	showErrorMessage={showErrorMessage}
+	{...emailFormItem}
+	/>
+
+	<TextBoxInput {...messageFormItem} />
+
+	<button on:click|preventDefault={submit} type="submit" class={formSent ? 'sent' : ''}>
+		{formSent ? 'Thank You' : 'Submit'}
+	</button>
 </form>
 
 <style>
--- src/components/contactForm/TextInput.svelte
+++ src/components/contactForm/TextInput.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	const dispatch = createEventDispatcher();
+
 	export let fieldName: string;
 	export let value: string;
+	export let showErrorMessage: boolean = false;
+	export let errorMessage: string = `Invalid ${fieldName}`;
+	let duration = 500;
+
+	const updateState = () => {
+		dispatch('input', {
+			isUserTyping: true
+		});
+	};
 </script>
 
 <div>
 	<label for={`${fieldName}-id`}>{fieldName}: </label>
-	<input type="text" id={`${fieldName}-id`} bind:value />
+	<input id={`${fieldName}-id`} on:input|preventDefault={updateState} type="text" bind:value />
+
+	{#if showErrorMessage}
+		<p class="error-message" transition:fade="{{ duration }}">
+			{errorMessage}
+		</p>
+	{/if}
 </div>
 
 <style>
@@ -17,5 +37,11 @@
 	}
 	label {
 		padding-left: 1rem;
+	}
+	.error-message {
+		margin: 0;
+		padding: 1rem 0rem 1rem 1rem;
+		color: #6b0000;
+		border: 2px solid #6b0000;
 	}
 </style>
